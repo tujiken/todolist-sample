f5115134a317703ecf9c77f8f25ed0789fcbbf6b
Tsujiken <tsujitakenta@MacBook-Air.local>
2023-09-10 13:36:50 +0900

    todoにidを付与

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 const taskValue = document.getElementsByClassName("task_value")[0];
 const taskSubmit = document.getElementsByClassName("task_submit")[0];
 const taskList = document.getElementsByClassName("task_list")[0];
-
+let number = 1;
 // 追加ボタンを作成
 const addTasks = (task) => {
   // 入力したタスクを追加・表示
   const listItem = document.createElement("li");
-  const showItem = taskList.appendChild(listItem);
-  showItem.innerHTML = task;
+  taskList.appendChild(listItem);
+  const taskName = document.createElement("p");
+  taskName.setAttribute("id", number);
+  listItem.appendChild(taskName);
+  taskName.textContent = task;
 
   // タスクに編集ボタンを追加・表示
   const editButton = document.createElement("button");
+  editButton.setAttribute("onclick", "editTask(" + number + ")");
   editButton.innerHTML = "Edit";
+  // クラスをつける
+  editButton.classList.add("edit");
   listItem.appendChild(editButton);
 
   // タスクに削除ボタンを付与
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Delete";
+  deleteButton.classList.add("delete");
   listItem.appendChild(deleteButton);
-
-  //編集ボタンをクリックし、イベントを発動（タスクを編集）
-  editButton.addEventListener("click", () => {
-    editTasks();
-  });
-
+  // クラスをつける
+  number++;
   // 削除ボタンをクリックし、イベントを発動（タスクが削除）
   deleteButton.addEventListener("click", (evt) => {
     evt.preventDefault();
@@ -38,17 +41,20 @@ const addTasks = (task) => {
   toggleClearButton();
 };
 //編集ボタンにタスクを編集する機能を追加
-const editTasks = () => {
+const editTask = (event) => {
   const editInput = document.getElementsByClassName("task_list");
-  const editTask = editInput.li;
-  console.log(editTask);
+  const parent = event.parentElement;
+  const taskName = document.getElementById(event);
+  taskName.textContent = taskValue.value;
+  console.log(event);
+  // console.log(parent);
   // for (let task = 0; task < editTask.length; task++) {
   //   console.log(task);
   // }
   // const editItem = editInput.getElementsByTagName("li")[0];
   // const editText = editItem.textContent;
   // editInput.value = taskText.textContent;
-  console.log(editInput);
+  // console.log(editInput);
 };
 
 // 削除ボタンにタスクを消す機能を付与
@@ -87,6 +93,3 @@ const toggleClearButton = () => {
     clearButton.style.display = "none";
   }
 };
-// console.log(taskValue);
-// console.log(taskSubmit);
-// console.log(taskList);
